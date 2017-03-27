faa4b507f2964e27b194e06d083ecb038f691d8b
nothing <hsudb@live.shop.edu.cn>
2017-03-27 11:03:36 +0800

    Update calc.sh

--- calc.sh
+++ calc.sh
@@ -6,6 +6,10 @@
 # 2017/03/10 Hsu
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin 
 export PATH
-netstat -an | grep 256| grep ESTABLISHED | awk -F ':' '{print $2"\t"}'|awk '{print $2}'| awk '!a[$0]++'|sort|uniq|while read line;
-do curl http://www.ip.cn/index.php?ip=$line;
+
+netstat -an | grep 256| grep ESTABLISHED | awk -F ':' '{print $2"\t"}' | awk '{print $2}'| awk '!a[$0]++'| sort | uniq | while read line
+
+do
+    sleep 0.3
+    curl --connect-timeout 1  http://ip.cn/index.php?ip=$line;
 done
