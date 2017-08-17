7d5b3e8e69c4f123d93659fa8f0b630f5bbf9b07
nothing <hsudb@live.shop.edu.cn>
2017-08-17 10:05:09 +0800

    Create gv.sh

---
+++ gv.sh
@@ -0,0 +1,16 @@
+contains() {
+string="$1"
+substring="$2"
+if test "${string#*$substring}" != "$string"
+then
+return 0 # $substring is in $string
+else
+return 1 # $substring is not in $string
+fi
+}
+
+while true; do
+result=$(curl -s）
+contains "$result" "error" || exit 0
+sleep 1.5
+done
