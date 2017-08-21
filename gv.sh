19f012cc2d9e321173d1f213ea2a7cf2bacd69a9
nothing <hsudb@live.shop.edu.cn>
2017-08-21 12:26:01 +0800

    Update gv.sh

--- gv.sh
+++ gv.sh
@@ -1,16 +1,17 @@
-contains() {
-string="$1"
-substring="$2"
-if test "${string#*$substring}" != "$string"
-then
-return 0 # $substring is in $string
-else
-return 1 # $substring is not in $string
-fi
+contains() 
+{
+ string="$1"
+ substring="$2"
+ if test "${string#*$substring}" != "$string"
+     then
+         return 0 # $substring is in $string
+     else
+         return 1 # $substring is not in $string
+ fi
 }
 
 while true; do
-result=$(curl -s）
-contains "$result" "error" || exit 0
-sleep 1.5
+      result=$(curl -s)
+      contains "$result" "error" || exit 0
+      sleep 1.5
 done
