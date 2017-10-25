15028cca9d7e56dfa20aeb1b8c075b2d90f33bee
nothing <hsudb@live.shop.edu.cn>
2017-10-25 13:31:53 +0800

    Update calc.sh

--- calc.sh
+++ calc.sh
@@ -7,7 +7,7 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin 
 export PATH
 
-netstat -an | grep 256| grep ESTABLISHED | awk -F ':' '{print $2"\t"}' | awk '{print $2}'| awk '!a[$0]++'| sort | uniq | while read line
+netstat -an | grep 701| grep ESTABLISHED | awk -F ':' '{print $2"\t"}' | awk '{print $2}'| awk '!a[$0]++'| sort | uniq | while read line
 
 do
     sleep 0.7
